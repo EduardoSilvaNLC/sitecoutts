75d9cb32e2c70442d20a207bb9b7d96597266243
Eduardo Silva <gestoreduardoleon@gmail.com>
2023-05-25 09:50:41 +0100

    updated sell links

--- src/routing/GoldLearn.tsx
+++ src/routing/GoldLearn.tsx
@@ -19,7 +19,7 @@ function GoldLearn() {
       </Box>
       <Box fontWeight="medium" color={colorText} display="contents">
         <Link
-          href="https://www.ebay.co.uk/itm/404286439961?mkcid=16&mkevt=1&mkrid=711-127632-2357-0&ssspo=_quk-6jtsnw&sssrc=2047675&ssuid=_quk-6jtsnw&widget_ver=artemis&media=COPY"
+          href="https://www.ebay.co.uk/itm/314610317863"
           isExternal
           fontSize={{ base: medio }}
         >
